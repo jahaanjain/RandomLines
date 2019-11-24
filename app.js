162d41a63b109ca6e343a9131864dbb546535ff9
Jahaan J <41234459+jahaanjain@users.noreply.github.com>
2019-11-24 13:33:34 -0500

    Added: Cooldowns, Ban-Phrase Checking
    Also better Database Handling and a new 'info' file for easier installation

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// RANDOM LINES BOT VERSION 1.0 BETA
+// RANDOM LINES BOT VERSION 2.0
 
-/* 
+/*
 Known Bugs:
-- lines.json resets on unexpected errors
+- N/A
 */
 
 // Module Loading
@@ -10,16 +10,15 @@ const tmi = require("tmi.js"); // Main Twitch Endpoint
 var fs = require('fs'); // Reading data for lines.json
 var request = require('request'); // API calls
 var moment = require('moment'); // Time since formatting
-
-/// EDIT THIS INFORMATION BELOW VVV
-var mUsername = `USERNAME_HERE`; // <- Enter the bot's username
-var mPassword = `oauth:ENTER_HERE`; // <- Enter the bot's oAuth code
-var mChannel = `ENTER_CHANNEL_NAME`; // <- Enter channel name (only add your own channel or a channel where you have broadcaster permission!)
-var debugMode = false; // <- Change to true for console logging on whenever the lines.json file changes (Can get spammy!)
-///
-
-// Only change information below if you know what you're doing!
-
+var userInfo = require("./info.json");
+//
+// User editable settings
+var mUsername = userInfo.info[0].mUsername;
+var mPassword = userInfo.info[0].mPassword;
+var mChannel = userInfo.info[0].mChannel;
+var debugMode = userInfo.info[0].debugMode;
+var cooldownSecs = userInfo.info[0].cooldown;
+var bannedWords = new Array(userInfo.info[0].bannedWords);
 
 let options = {
     options: {
@@ -35,6 +34,7 @@ let options = {
     },
     channels: [mChannel]
 };
+
 let client = new tmi.client(options);
 client.on('connected', onConnectedHandler);
 function callback(error, response, body) {
@@ -46,16 +46,22 @@ request(options, callback);
 function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
 }
-
+if (debugMode == true) { console.log(mUsername, mPassword, mChannel, debugMode); }
 
 // General Functions
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+async function getBannable(message) {
+  if (bannedWords.some(function(v) { return message.indexOf(v) >= 0; })) { return bannable = true; } else { return bannable = false; }
+}
+
+// global variables
+cooldownRQ = new Date();
+cooldownRL = new Date();
 
 client.connect();
 client.on("chat", function(channel, userstate, message, self) {
-    if (self) { return; }
 
     const commandName = message.trim(); var cmdSplit = commandName.split(' ');
 
@@ -63,15 +69,15 @@ client.on("chat", function(channel, userstate, message, self) {
 
         if (cmdSplit[1] === `eval`) {
           try {
-          var exfun = `${commandName.substring(12)}`;
-          console.log(exfun);
-          var output1 = eval(exfun).toString();
-          client.say(channel, output1);
-          console.log(eval(exfun));
+            var exfun = `${commandName.substring(12)}`;
+            console.log(exfun);
+            var output1 = eval(exfun).toString();
+            client.say(channel, output1);
+            console.log(eval(exfun));
           } catch(error) { console.log(error); }
           return;
         }
-  
+
     }
 
     try {
@@ -84,51 +90,89 @@ client.on("chat", function(channel, userstate, message, self) {
           message: `${commandName}`
         });
         fs.writeFile('./lines.json', JSON.stringify(linesDB, null, 2), 'utf-8', function(err) {
-          if (err) throw err
+          if (err) throw err;
           if (options.options.debug === true) { console.log('Message recorded!'); }
-        })
+        });
       });
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) { console.log(error); }
 
-    if (cmdSplit[0] === `!randomline` || cmdSplit[0] === `!rl`) {
-      try {
-        fs.readFile('./lines.json', (err, data) => {
-          if (err) throw err;
-          let linesDB = JSON.parse(data);
-          if (typeof cmdSplit[1] === 'undefined') {
-            var pos = linesDB.messages.map(function(e) {  return e.username; }).indexOf(userstate.username, Math.floor(Math.random()*linesDB.messages.length));
-            var messageRandom1 = linesDB.messages[pos];
-            if (options.options.debug === true) { console.log(messageRandom1); }
-            var messageRandom2 = `[Random Line] ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
-            client.action(channel, messageRandom2);
+    setTimeout(function() {
+      if (cmdSplit[0] === `!randomline` || cmdSplit[0] === `!rl`) { // gets a random message from person who ran command
+        if (((new Date().getTime() - new Date(cooldownRL).getTime()) / 1000) > cooldownSecs) {
+          try {
+            fs.readFile('./lines.json', (err, data) => {
+              if (err) throw err;
+              let linesDB = JSON.parse(data);
+              if (typeof cmdSplit[1] === 'undefined' || cmdSplit[1].length < 3) {
+                var pos = linesDB.messages.map(function(e) {  return e.username; }).indexOf(userstate.username, Math.floor(Math.random()*linesDB.messages.length));
+                var messageRandom1 = linesDB.messages[pos];
+                if (options.options.debug === true) { console.log(messageRandom1); }
+                var messageCheck = `${messageRandom1.message} ${messageRandom1.username}`;
+                getBannable(messageCheck).then(function(bannable) {
+                  if (bannable == false) {
+                    var messageRandom2 = `[Random Line] ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
+                    client.action(channel, messageRandom2);
+                  }
+                  else {
+                    var messageRandom2 = `[Random Line] ** BANNED PHRASE DETECTED ** `;
+                    client.action(channel, messageRandom2);
+                  }
+                }).catch(function(error) { console.log(error); client.action(channel, `Error while ban-phrase checking`) });
+              }
+              else if (cmdSplit[1].length < 3) {
+                var pos = linesDB.messages.map(function(e) { return e.username; }).indexOf(cmdSplit[1], Math.floor(Math.random()*linesDB.messages.length));
+                if (pos < 0) {
+                  if (cmdSplit[1].length < 3) { client.action(channel, `[Random Line] Could not find any messages for specified user.`); }
+                  else { client.action(channel, `[Random Line] Could not find any messages for ${cmdSplit[1]}`); }
+                } else {
+                  var messageRandom1 = linesDB.messages[pos];
+                  if (options.options.debug === true) { console.log(messageRandom1); }
+                  var messageCheck = `${messageRandom1.message} ${messageRandom1.username}`;
+                  getBannable(messageCheck).then(function(bannable) {
+                    if (bannable == false) {
+                      var messageRandom2 = `[Random Line] ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
+                      client.action(channel, messageRandom2);
+                    }
+                    else {
+                      var messageRandom2 = `[Random Line] ** BANNED PHRASE DETECTED ** `;
+                      client.action(channel, messageRandom2);
+                    }
+                  }).catch(function(error) { console.log(error); client.action(channel, `Error while ban-phrase checking`) });
+                }
+              }
+            });
+          } catch (error) {
+            console.log(error);
           }
-          else if (cmdSplit[1].length >= 2) {
-            var pos = linesDB.messages.map(function(e) {  return e.username; }).indexOf(cmdSplit[1], Math.floor(Math.random()*linesDB.messages.length));
-            var messageRandom1 = linesDB.messages[pos];
-            if (options.options.debug === true) { console.log(messageRandom1); }
-            var messageRandom2 = `[Random Line] From ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
-            client.action(channel, messageRandom2);
-          }
-        });
-      } catch (error) {
-        console.log(error);
+          cooldownRL = new Date();
+        }
       }
-    }
 
-    if (cmdSplit[0] === `!randomquote` || cmdSplit[0] === `!rq`) {
-      try {
-        fs.readFile('./lines.json', (err, data) => {
-          if (err) throw err;
-          let linesDB = JSON.parse(data);
-          var messageRandom1 = linesDB.messages[Math.floor(Math.random()*linesDB.messages.length)];
-          var messageRandom2 = `[Random Quote] From ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
-          client.action(channel, messageRandom2);
-        });
-      } catch (error) {
-        console.log(error);
+      if (cmdSplit[0] === `!randomquote` || cmdSplit[0] === `!rq`) { // gets random message from anyone in chat
+        if (((new Date().getTime() - new Date(cooldownRQ).getTime()) / 1000) > cooldownSecs) {
+          try {
+            fs.readFile('./lines.json', (err, data) => {
+              if (err) throw err;
+              let linesDB = JSON.parse(data);
+              var messageRandom1 = linesDB.messages[Math.floor(Math.random()*linesDB.messages.length)];
+              var messageCheck = `${messageRandom1.message} ${messageRandom1.username}`;
+              getBannable(messageCheck).then(function(bannable) {
+                if (bannable == false) {
+                  var messageRandom2 = `[Random Quote] ${messageRandom1.username} (${moment(messageRandom1.date).fromNow()}): ${messageRandom1.message}`;
+                  client.action(channel, messageRandom2);
+                }
+                else {
+                  var messageRandom2 = `[Random Quote] ** BANNED PHRASE DETECTED ** `;
+                  client.action(channel, messageRandom2);
+                }
+              }).catch(function(error) { console.log(error); client.action(channel, `Error while ban-phrase checking`) });
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          cooldownRQ = new Date();
+        }
       }
-    }
+    }, 50);
 
 });
